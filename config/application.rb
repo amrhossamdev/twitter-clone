43d927cfa3e1946aabd0985381c8e35331686ee9
Amr Hossam <amrhossam.dev@gmail.com>
2024-09-14 23:50:06 +0300

    update app.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SimpleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.autoload_paths += Dir["#{config.root}/app/services"]
 
 
   end

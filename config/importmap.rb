9a580631274d5d10f583ea5a6d6d7140ae974c58
Amr Hossam <amrhossam.dev@gmail.com>
2024-09-15 20:43:01 +0300

    update menu

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.9
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/custom",      under: "custom"
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.6
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.100

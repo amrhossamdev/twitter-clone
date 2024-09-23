1b350c35cc79e914ebd917a88d0b0498514d1574
Amr Hossam <amrhossam.dev@gmail.com>
2024-09-23 04:53:53 +0300

    refactor mailer settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# app/mailers/application_mailer.rb
+
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["email_address"]
+  default from: Rails.env.test? ? "noreply@example.com" : ENV["FROM_MAIL_ADDRESS"]
   layout "mailer"
 end

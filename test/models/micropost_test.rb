302573a2ff6a4767929bee79aadd685216843e98
Amr Hossam <amrhossam.dev@gmail.com>
2024-09-25 09:28:40 +0300

    feat: add micropost

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class MicropostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:amr)
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = " "
+    assert_not @micropost.valid?
+  end
+
+end
